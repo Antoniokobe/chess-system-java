0f7692dd7d367663276b802729a3195738c660f3
Antonio Carlos <antonioconceicao@outlook.com>
2025-08-23 20:05:53 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	protected Position position;
 	private Board board;
 	
@@ -15,6 +15,30 @@ public class Piece {
 	}
 
 	
+	/* O hook method é uma parte fundamental do Padrão Template Method, 
+	onde uma classe abstrata define o esqueleto de um algoritmo e, 
+	através dos ganchos, permite que as subclasses substituam etapas
+    específicas desse algoritmo.*/
+	
+	public abstract boolean[][] possibleMoves();
+		
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+		
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean [][] mat =possibleMoves();
+		for(int i=0; i<mat.length;i++) {
+			for(int j=0; j<mat.length;j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+		
+	}
 	
 	
 	
